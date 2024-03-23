57f120aed1962cb1c55dc0ebee00b8f6b5e32176
quyduong2403 <149467594+quyduong2403@users.noreply.github.com>
2024-03-23 13:53:06 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,24 @@
-public class Book {
+public class Book extends Item {
+    private final String author;
+    private final int numberOfPages;
+    private final String genre;
+
+    public Book(String name, String description, String ID, float price, String author, int numberOfPages, String genre) {
+        // Gọi constructor của lớp cha (Item)
+        super(name, description, ID, price);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.genre = genre;
+    }
+    @Override
+    public void showInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Description: " + getDescription());
+        System.out.println("ID: " + ID);
+        System.out.println("Price: " + price);
+        System.out.println("Author: " + author);
+        System.out.println("Number of Pages: " + numberOfPages);
+        System.out.println("Genre: " + genre);
+    }
 }
+
